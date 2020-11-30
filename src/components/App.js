ef0542eaec2465dec809ac5fd4122880aaf4d464
anekarupa <anekarupa26@gmail.com>
2020-11-30 15:22:27 +0700

    add more pages

--- src/components/App.js
+++ src/components/App.js
@@ -13,6 +13,8 @@ export default function App() {
         <Switch>
           <Route exact path="/" component={() => <div>Beranda</div> } />
           <Route exact path="/layanan" component={() => <div>Layanan</div> } />
+          <Route exact path="/kuesioner" component={() => <div>Layanan - Kuesioner</div> } />
+          <Route exact path="/monitoring" component={() => <div>Layanan - Monitoring</div> } />
           <Route exact path="/tentang" component={() => <div>Tentang</div> } />
           <Route exact path="/kontak" component={() => <div>Kontak</div> } />
           <Route exact path="/masuk" component={() => <div>Masuk</div> } />
