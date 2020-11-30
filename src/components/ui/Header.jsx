c6ec01ea00ecbc18e4517104a9f3ab189439a4f4
anekarupa <anekarupa26@gmail.com>
2020-11-30 10:44:13 +0700

    add toolbar margin

--- src/components/ui/Header.jsx
+++ src/components/ui/Header.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
-
 import useScrollTrigger from '@material-ui/core/useScrollTrigger';
+import { makeStyles } from '@material-ui/core/styles';
 
 function ElevationScroll(props) {
   const { children } = props;
@@ -16,14 +16,25 @@ function ElevationScroll(props) {
   });
 }
 
+const useStyles = makeStyles(theme => ({
+  toolbarMargin: {
+    ...theme.mixins.toolbar
+  }
+}))
+
 export default function Header(props) {
+  const classes = useStyles()
+
   return (
-    <ElevationScroll>
-      <AppBar>
-        <Toolbar>
-          Paperon Dev
+    <React.Fragment>
+      <ElevationScroll>
+        <AppBar>
+          <Toolbar>
+            Paperon Dev
          </Toolbar>
-      </AppBar>
-    </ElevationScroll>
+        </AppBar>
+      </ElevationScroll>
+      <div className={classes.toolbarMargin} />
+    </React.Fragment>
   )
 }
