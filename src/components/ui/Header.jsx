cd4694c9fe653ceb4e14817d81df2c2e7cff7b23
anekarupa <anekarupa26@gmail.com>
2020-11-30 11:19:24 +0700

    add tabs and tab

--- src/components/ui/Header.jsx
+++ src/components/ui/Header.jsx
@@ -3,6 +3,8 @@ import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import useScrollTrigger from '@material-ui/core/useScrollTrigger';
 import { makeStyles } from '@material-ui/core/styles';
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
 
 import logo from '../../assets/logo.svg'
 
@@ -25,6 +27,14 @@ const useStyles = makeStyles(theme => ({
   },
   logo: {
     height: '7em'
+  },
+  tabContainer: {
+    marginLeft: 'auto'
+  },
+  tab: {
+    ...theme.typography.tab,
+    minWidth: 10,
+    marginLeft: '25px'
   }
 }))
 
@@ -41,6 +51,12 @@ export default function Header(props) {
               alt="my logo"
               className={classes.logo}
             />
+            <Tabs className={classes.tabContainer}>
+              <Tab className={classes.tab} label="Beranda" />
+              <Tab className={classes.tab} label="Layanan" />
+              <Tab className={classes.tab} label="Tentang" />
+              <Tab className={classes.tab} label="Kontak" />
+            </Tabs>
           </Toolbar>
         </AppBar>
       </ElevationScroll>
