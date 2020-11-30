681fe302df84f270f21f8e212cf4eeb5909bb6aa
anekarupa <anekarupa26@gmail.com>
2020-11-30 16:15:34 +0700

    add drawer

--- src/components/ui/Header.jsx
+++ src/components/ui/Header.jsx
@@ -11,6 +11,9 @@ import Menu from '@material-ui/core/Menu';
 import MenuItem from '@material-ui/core/MenuItem';
 import useMediaQuery from '@material-ui/core/useMediaQuery'
 import { useTheme } from '@material-ui/core/styles'
+import SwipeableDrawer from '@material-ui/core/SwipeableDrawer'
+import MenuIcon from '@material-ui/icons/Menu'
+import IconButton from '@material-ui/core/IconButton';
 
 import logo from '../../assets/logo.svg'
 
@@ -82,6 +85,16 @@ const useStyles = makeStyles(theme => ({
     "&:hover": {
       opacity: 1
     }
+  },
+  drawerIconContainer: {
+    marginLeft: 'auto',
+    "&:hover": {
+      backgroundColor: 'transparent'
+    }
+  },
+  drawerIcon: {
+    height: '35px',
+    width: '35px'
   }
 }))
 
@@ -89,17 +102,20 @@ export default function Header(props) {
   const classes = useStyles()
   const theme = useTheme()
   const matches = useMediaQuery(theme.breakpoints.down("md"))
+  const iOS = process.browser && /iPad|iPhone|iPod/.test(navigator.userAgent);
+
   const [anchorEl, setAnchorEl] = useState(null)
-  const [open, setOpen] = useState(false)
+  const [openMenu, setOpenMenu] = useState(false)
+  const [openDrawer, setOpenDrawer] = useState(false)
 
   const handleOpen = (e) => {
     setAnchorEl(e.currentTarget)
-    setOpen(true)
+    setOpenMenu(true)
   }
 
   const handleClose = (e) => {
     setAnchorEl(null)
-    setOpen(false)
+    setOpenMenu(false)
   }
 
   const tabs = (
@@ -113,11 +129,22 @@ export default function Header(props) {
       <Button variant="contained" color="secondary" className={classes.button}>
         Masuk
             </Button>
-      <Menu id="simple-menu" anchorEl={anchorEl} open={open} onClose={handleClose} MenuListProps={{ onMouseLeave: handleClose }} classes={{ paper: classes.menu }} elevation={0} >
+      <Menu id="simple-menu" anchorEl={anchorEl} open={openMenu} onClose={handleClose} MenuListProps={{ onMouseLeave: handleClose }} classes={{ paper: classes.menu }} elevation={0} >
         <MenuItem onClick={handleClose} component={Link} to="/layanan" classes={{ root: classes.menuItem }} >Layanan</MenuItem>
         <MenuItem onClick={handleClose} component={Link} to="/kuesioner" classes={{ root: classes.menuItem }} >Kuesioner</MenuItem>
         <MenuItem onClick={handleClose} component={Link} to="/monitoring" classes={{ root: classes.menuItem }} >Monitoring</MenuItem>
       </Menu>
+    </React.Fragment>
+  )
+
+  const drawer = (
+    <React.Fragment>
+      <SwipeableDrawer disableBackdropTransition={!iOS} disableDiscovery={iOS} open={openDrawer} onClose={() => setOpenDrawer(false)} onOpen={() => setOpenDrawer(true)} >
+        Example Drawer
+      </SwipeableDrawer>
+      <IconButton onClick={() => setOpenDrawer(!openDrawer)} disableRipple className={classes.drawerIconContainer} >
+        <MenuIcon className={classes.drawerIcon} />
+      </IconButton>
     </React.Fragment>
   )
 
@@ -133,7 +160,7 @@ export default function Header(props) {
                 className={classes.logo}
               />
             </Button>
-            {matches ? null : tabs}
+            {matches ? drawer : tabs}
           </Toolbar>
         </AppBar>
       </ElevationScroll>
