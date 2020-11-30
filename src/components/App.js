df2889fc2a5819529ed4395bc9e1af7f4e70f61d
anekarupa <anekarupa26@gmail.com>
2020-11-30 10:34:45 +0700

    add theme provider

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,14 @@
 import React from 'react'
+import { ThemeProvider } from '@material-ui/core/styles';
+
+import theme from './ui/Theme'
 import Header from './ui/Header'
 
 export default function App() {
   return (
-    <div>
+    <ThemeProvider theme={theme}>
       <Header />
       Hello Paperon Dev
-    </div>
+    </ThemeProvider>
   );
 }
