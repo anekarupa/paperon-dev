8472e4f1c72e643907c903fb73539eec4dc68c9d
anekarupa <anekarupa26@gmail.com>
2020-11-30 19:47:17 +0700

    Update Footer.jsx

--- src/components/ui/Footer.jsx
+++ src/components/ui/Footer.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import { makeStyles } from '@material-ui/core/styles';
+import Grid from '@material-ui/core/Grid'
 
 import footerAdornment from '../../assets/Footer Adornment.svg'
 
@@ -19,13 +21,64 @@ const useStyles = makeStyles(theme => ({
     [theme.breakpoints.down('xs')]: {
       width: '12em'
     },
+  },
+  mainContainer: {
+    position: 'absolute'
+  },
+  link: {
+    color: 'white',
+    fontFamily: 'Arial',
+    fontSize: '0.75rem',
+    fontWeight: 'bold',
+    textDecoration: 'none'
+  },
+  gridItem: {
+    margin: '3em'
   }
 }))
 
 export default function Footer() {
   const classes = useStyles()
 
-  return <footer className={classes.footer}>
-    <img alt="footer adornment" src={footerAdornment} className={classes.adornment} />
-  </footer>
+  return (
+    <footer className={classes.footer}>
+      <Grid container justify="center" className={classes.mainContainer} >
+        <Grid item className={classes.gridItem}>
+          <Grid container direction="column" spacing={2} >
+            <Grid item component={Link} to="/" className={classes.link} >
+              Beranda
+            </Grid>
+          </Grid>
+        </Grid>
+        <Grid item className={classes.gridItem}>
+          <Grid container direction="column" spacing={2} >
+            <Grid item component={Link} to="/layanan" className={classes.link} >
+              Layanan
+            </Grid>
+            <Grid item component={Link} to="/kuesioner" className={classes.link} >
+              Kuesioner
+            </Grid>
+            <Grid item component={Link} to="/monitoring" className={classes.link} >
+              Monitoring
+            </Grid>
+          </Grid>
+        </Grid>
+        <Grid item className={classes.gridItem}>
+          <Grid container direction="column" spacing={2} >
+            <Grid item component={Link} to="/tentang" className={classes.link}>
+              Tentang
+            </Grid>
+          </Grid>
+        </Grid>
+        <Grid item className={classes.gridItem}>
+          <Grid container direction="column" spacing={2} >
+            <Grid item component={Link} to="/kontak" className={classes.link}>
+              Kontak
+            </Grid>
+          </Grid>
+        </Grid>
+      </Grid>
+      <img alt="footer adornment" src={footerAdornment} className={classes.adornment} />
+    </footer>
+  )
 }
